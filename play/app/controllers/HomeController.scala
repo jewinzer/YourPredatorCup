8178bd119baad9dadca26916affd3aab2a8fb1bb
jewinzer <jewinzer@gmail.com>
2021-05-09 02:22:50 +0200

    wider

--- play/app/controllers/HomeController.scala
+++ play/app/controllers/HomeController.scala
@@ -1,24 +1,36 @@
 package controllers
 
+import daos.ProduktDAO
+import model.Produkt
+
 import javax.inject._
 import play.api._
+import play.api.data.Form
+import play.api.data.Forms.{mapping, number, text}
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
-@Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+import scala.concurrent.ExecutionContext
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+@Singleton
+class HomeController @Inject() (produktDao: ProduktDAO, controllerComponents: ControllerComponents)
+                               (implicit executionContext: ExecutionContext) extends AbstractController(controllerComponents) {
+
+  def index() = Action.async {
+    produktDao.all().map { case (produkte) => Ok(views.html.index(produkte)) }
+  }
+
+  def env() = Action { implicit request: Request[AnyContent] =>
+    //Ok("Nothing to see here")
+    Ok(System.getenv("JDBC_DATABASE_URL"))
+  }
+
+  val produktForm = Form(
+    mapping(
+      "name" -> text(),
+      "price" -> number())(Produkt.apply)(Produkt.unapply))
+
+  def insertProdukt = Action.async { implicit request =>
+    val produkt: Produkt = produktForm.bindFromRequest.get
+    produktDao.insert(produkt).map(_ => Redirect(routes.HomeController.index))
   }
 }
--- play/build.sbt
+++ play/build.sbt
@@ -9,6 +9,20 @@ scalaVersion := "2.13.5"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+libraryDependencies += "org.postgresql" % "postgresql" % "42.2.5"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.play" %% "play-slick" % "5.0.0",
+  "com.typesafe.play" %% "play-slick-evolutions" % "5.0.0"
+)
+Compile / herokuProcessTypes := Map(
+  "web" -> "target/universal/stage/bin/play-app-test -Dhttp.port=$PORT",
+)
+
+
+
+
+
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "winzer.controllers._"
